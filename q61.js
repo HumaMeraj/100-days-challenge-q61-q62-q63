70e5ca3d0263d9e88bc622047dfae8aca6a076a0
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-04 03:01:56 +0500

    File of q61

---
+++ q61.js
@@ -0,0 +1,10 @@
+// Making Enums for Vehicles:
+// Making a list (enum) for different types of vehicles
+var VehicleType;
+(function (VehicleType) {
+    VehicleType[VehicleType["Car"] = 0] = "Car";
+    VehicleType[VehicleType["Truck"] = 1] = "Truck";
+    VehicleType[VehicleType["Motorcycle"] = 2] = "Motorcycle";
+})(VehicleType || (VehicleType = {}));
+// Showing one type of vehicle from the list
+console.log(VehicleType.Motorcycle);
---
+++ q61.ts
@@ -0,0 +1,15 @@
+// Making Enums for Vehicles:
+
+// Making a list (enum) for different types of vehicles
+
+enum VehicleType {
+    Car,
+    Truck,
+    Motorcycle
+}
+
+// Showing one type of vehicle from the list
+console.log(VehicleType.Motorcycle);
+// It shows 0 because enums start counting from 0
+// Here, we're just checking what number the Car category got in our list.
+
